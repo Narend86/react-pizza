e2411b64d1b903677ed3d5966a09e2a70d6d81aa
Narendra <31529394+Narend86@users.noreply.github.com>
2026-09-18 12:44:28 +0530

    conflict resolve in redme file

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { getUser } from "./userSlice";
+
+function UserName() {
+  const username = useSelector(getUser);
+  if(!username) return null;
+  return <div className="hidden text-sm font-semibold md:block">{username}</div>;
+}
+
+export default UserName;
